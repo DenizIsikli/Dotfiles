29eff2c4c46d52309ce540785bdc1b81f9efeb31
Bobby <s215818@dtu.dk>
2024-02-27 03:20:02 +0100

    Markdown preview plugin

--- init.lua
+++ init.lua
@@ -11,9 +11,8 @@ require('packer').startup(function()
         branch = "v3.x",
         requires = { 
         "nvim-lua/plenary.nvim",
-        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+        "nvim-tree/nvim-web-devicons",
         "MunifTanjim/nui.nvim",
-        -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
     }
   }
 
@@ -86,14 +85,12 @@ require('packer').startup(function()
 
   -- Markdown Preview
   use({
-      "iamcco/markdown-preview.nvim",
-      run = function() vim.fn["mkdp#util#install"]() end,
+    "iamcco/markdown-preview.nvim",
+    run = function() vim.fn["mkdp#util#install"]() end,
   })
-
-  use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
 end)
 
--- Setting the default directory to start in nvim configuration directory
+-- Setting the default directory to start in Neovim projects folder
 vim.cmd [[cd C:\Users\deniz\Neovim]]
 
 -- Project Directory
