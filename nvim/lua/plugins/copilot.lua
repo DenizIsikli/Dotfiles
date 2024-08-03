795c1dc60c5b4822485a14065a1d7cdae8f2f688
Deniz Isikli <s215818@dtu.dk>
2024-08-03 22:45:50 +0200

    Copilot toggle function

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -4,5 +4,18 @@ return {
   config = function()
     vim.g.copilot_no_tab_map = true
     vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true, script = true })
+
+    vim.api.nvim_create_user_command('CopilotToggle', function ()
+        vim.g.copilot_enabled = not vim.g.copilot_enabled
+        if vim.g.copilot_enabled then
+            vim.cmd('Copilot disable')
+            print("Copilot Disabled")
+        else
+            vim.cmd('Copilot enable')
+            print("Copilot Enabled")
+        end
+    end, { nargs = 0})
+
+    vim.api.nvim_set_keymap("n", "<leader>ct", ":CopilotToggle<CR>", { noremap = true, silent = true })
   end,
 }
