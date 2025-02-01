96f0b13acee2a0a9d19316a56dd082693c612174
Deniz <s215818@dtu.dk>
2025-02-01 01:56:21 +0100

    Fix

--- nvim/deployWindows.ps1
+++ nvim/deployWindows.ps1
@@ -2,22 +2,26 @@
 
 # # To run the script: .\deployWindows.ps1
 # Source paths
-SOURCE_PATH="C:\Users\deniz\AppData\Local\nvim"
-INIT_FILE="$SOURCE_PATH\init.lua"
-LUA_FOLDER="$SOURCE_PATH\lua"
-PACK_FOLDER="$SOURCE_PATH\pack"
+$SOURCE_PATH = "C:\Users\deniz\AppData\Local\nvim"
+$INIT_FILE = "$SOURCE_PATH\init.lua"
+$LUA_FOLDER = "$SOURCE_PATH\lua"
+$PACK_FOLDER = "$SOURCE_PATH\pack"
 
 # Destination paths
-DEST_PATH="C:\Users\deniz\Desktop\Code\Dotfiles\nvim"
+$DEST_PATH = "C:\Users\deniz\Desktop\Code\Dotfiles\nvim"
+
+# Ensure the destination folder exists
+if (-Not (Test-Path -Path $DEST_PATH)) {
+    New-Item -ItemType Directory -Path $DEST_PATH -Force
+}
 
 # Copy the init.lua file
-cp "$INIT_FILE" "$DEST_PATH"
+Copy-Item -Path $INIT_FILE -Destination $DEST_PATH -Force
 
 # Copy the lua folder
-cp -r "$LUA_FOLDER" "$DEST_PATH"
+Copy-Item -Path $LUA_FOLDER -Destination $DEST_PATH -Recurse -Force
 
 # Copy the pack folder
-cp -r "$PACK_FOLDER" "$DEST_PATH"
+Copy-Item -Path $PACK_FOLDER -Destination $DEST_PATH -Recurse -Force
 
-echo "Deployment completed"
-
+Write-Output "Deployment completed"
