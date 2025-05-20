90a7364ec99630b41e4a425d0d1141c9f78b6d10
Deniz Isikli <s215818@dtu.dk>
2025-05-21 00:12:55 +0200

    Fixed flash binding overwriting existing one

--- nvim/lua/plugins/flash.lua
+++ nvim/lua/plugins/flash.lua
@@ -2,7 +2,13 @@
 return {
     "folke/flash.nvim",
     event = "VeryLazy",
-    opts = {},
+    opts = {
+        modes = {
+            char = {
+                enabled = false, -- disables default 's' and 't' mappings in normal/visual/operator modes
+            },
+        },
+    },
     keys = {
         { "<leader>fs", mode = "n", function() require("flash").jump() end, desc = "Flash Search" },
     },
