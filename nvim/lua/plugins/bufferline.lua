dd9918e08d3393b273ecf79c61a8a3c6fe08cb3a
Deniz Isikli <s215818@dtu.dk>
2025-06-02 22:38:37 +0200

    Bufferline fix

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -7,12 +7,8 @@ return {
     require("bufferline").setup({
       options = {
         mode = "buffers",
-        numbers = "ordinal",
         diagnostics = "nvim_lsp",
-        show_buffer_icons = true,
-        show_buffer_close_icons = true,
         show_close_icon = false,
-        separator_style = "thin",
         always_show_bufferline = true,
         offsets = {
           {
@@ -22,60 +18,14 @@ return {
             text_align = "left",
           }
         },
-        name_formatter = function(buf)
-          return vim.fn.fnamemodify(buf.name, ":t")
+        custom_filter = function(bufnr)
+          for _, win in ipairs(vim.api.nvim_list_wins()) do
+            if vim.api.nvim_win_get_buf(win) == bufnr then
+              return true
+            end
+          end
+          return false
         end,
-      },
-      highlights = {
-        fill = {
-          bg = "#1d2021",
-        },
-        background = {
-          fg = "#a89984",
-          bg = "#1d2021",
-        },
-        buffer = {
-          fg = "#a89984",
-          bg = "#1d2021",
-        },
-        buffer_visible = {
-          fg = "#ebdbb2",
-          bg = "#1d2021",
-        },
-        buffer_selected = {
-          fg = "#fbf1c7",
-          bg = "#3c3836",
-          gui = "bold",
-        },
-
-        numbers = {
-          fg = "#928374",
-          bg = "#1d2021",
-        },
-        numbers_selected = {
-          fg = "#ffffff",
-          bg = "#3c3836",
-          gui = "bold,underline",
-        },
-
-        -- Dark gray vertical separators (no bright edges)
-        separator = {
-          fg = "#3c3836",
-          bg = "#1d2021",
-        },
-        separator_visible = {
-          fg = "#3c3836",
-          bg = "#1d2021",
-        },
-        separator_selected = {
-          fg = "#3c3836",
-          bg = "#3c3836",
-        },
-
-        indicator_selected = {
-          fg = "#b8bb26",
-          bg = "#3c3836",
-        },
       },
     })
   end,
