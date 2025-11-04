9d685e56bcd03d9f92afbb1885a01f8412033b92
Deniz Isikli <s215818@dtu.dk>
2025-11-04 23:49:49 +0100

    nvim -v 0.11.x lsp fix

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,103 +1,120 @@
--- Mason, LSP and Completion Configuration
+local lspKeys = function(client, bufnr)
+  local base_opts = { noremap = true, silent = true, buffer = bufnr }
+  local function opts(desc) return vim.tbl_extend('error', base_opts, { desc = desc }) end
+
+  local mappings = {
+    { mode = 'n', key = '<leader>gd', fn = vim.lsp.buf.definition, desc = 'Go to definition' },
+    { mode = 'n', key = '<leader>gi', fn = vim.lsp.buf.implementation, desc = 'Go to implementation' },
+    { mode = 'n', key = '<leader>gr', fn = vim.lsp.buf.references, desc = 'Find references' },
+    { mode = 'n', key = '<leader>K', fn = vim.lsp.buf.hover, desc = 'Hover' },
+    { mode = 'n', key = '<leader>rn', fn = vim.lsp.buf.rename, desc = 'Rename symbol' },
+    { mode = 'n', key = 'gdh', fn = function() vim.cmd('vsplit') vim.lsp.buf.definition() end, desc = 'Definition (vsplit)' },
+    { mode = 'n', key = 'gdl', fn = function() vim.cmd('split') vim.lsp.buf.definition() end, desc = 'Definition (split)' },
+    { mode = 'n', key = 'gih', fn = function() vim.cmd('vsplit') vim.lsp.buf.implementation() end, desc = 'Implementation (vsplit)' },
+    { mode = 'n', key = 'gil', fn = function() vim.cmd('split') vim.lsp.buf.implementation() end, desc = 'Implementation (split)' },
+  }
+
+  for _, map in ipairs(mappings) do
+    vim.keymap.set(map.mode or 'n', map.key, map.fn, opts(map.desc))
+  end
+
+  if client.supports_method('inlayHintProvider') then
+    vim.keymap.set('n', '<leader>ih', function()
+      vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = bufnr }), { bufnr = bufnr })
+    end, opts('Toggle inlay hints'))
+  end
+end
+
 return {
-  -- Mason core
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
+  'neovim/nvim-lspconfig',
+  dependencies = {
+    'williamboman/mason.nvim',
+    'williamboman/mason-lspconfig.nvim',
+    'SmiteshP/nvim-navic',
+    'hrsh7th/nvim-cmp',
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-path',
+    'L3MON4D3/LuaSnip',
+    'saadparwaiz1/cmp_luasnip',
   },
+  lazy = false,
+  config = function()
+    local servers = {
+      'lua_ls',
+      'ts_ls',
+      'html',
+      'cssls',
+      'jsonls',
+      'pyright',
+      'clangd',
+      'tailwindcss',
+      'rust_analyzer',
+      'zls',
+    }
 
-  -- Mason LSP integration
-  {
-    "williamboman/mason-lspconfig.nvim",
-    dependencies = { "williamboman/mason.nvim", "neovim/nvim-lspconfig" },
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-          local on_attach = function(_, bufnr)
-                local map = function(mode, lhs, rhs)
-                    vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, noremap = true, silent = true })
-                end
-                map('n', 'gd', vim.lsp.buf.definition)
-                map('n', 'gi', vim.lsp.buf.implementation)
-                map('n', 'gr', vim.lsp.buf.references)
-                map('n', 'K', vim.lsp.buf.hover)
-                map('n', '<leader>rn', vim.lsp.buf.rename)
-                map('n', 'gdh', function() vim.cmd('vsplit') vim.lsp.buf.definition() end)
-                map('n', 'gdl', function() vim.cmd('split') vim.lsp.buf.definition() end)
-                map('n', 'gih', function() vim.cmd('vsplit') vim.lsp.buf.implementation() end)
-                map('n', 'gil', function() vim.cmd('split') vim.lsp.buf.implementation() end)
+    require('mason').setup()
+    require('mason-lspconfig').setup({
+      ensure_installed = servers,
+    })
+
+    local capabilities = require('cmp_nvim_lsp').default_capabilities()
+    vim.lsp.config('*', {
+      capabilities = capabilities,
+    })
+
+    vim.lsp.enable(servers)
+
+    local lsp_group = vim.api.nvim_create_augroup('UserLspAttach', { clear = true })
+    vim.api.nvim_create_autocmd('LspAttach', {
+      group = lsp_group,
+      desc = 'Setup keymaps and navic on LSP attach',
+      callback = function(args)
+        local bufnr = args.buf
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if not client then return end
+        lspKeys(client, bufnr)
+
+        if client.server_capabilities.completionProvider then
+          vim.bo[bufnr].omnifunc = 'v:lua.vim.lsp.omnifunc'
+          vim.bo[bufnr].formatexpr = 'v:lua.vim.lsp.formatexpr()'
+        end
+
+        if client.server_capabilities.documentSymbolProvider then
+          require('nvim-navic').attach(client, bufnr)
+        end
+      end,
+    })
+
+    -- Completion setup
+    local cmp = require('cmp')
+    local luasnip = require('luasnip')
+    require('luasnip.loaders.from_vscode').lazy_load()
+
+    cmp.setup({
+      snippet = {
+        expand = function(args) luasnip.lsp_expand(args.body) end,
+      },
+      mapping = cmp.mapping.preset.insert({
+        ['<Tab>'] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          else
+            fallback()
           end
-
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "lua_ls",
-          "ts_ls",
-          "html",
-          "cssls",
-          "jsonls",
-          "pyright",
-          "clangd",
-          "tailwindcss",
-          "rust_analyzer",
-          "zls",
-        },
-        handlers = {
-          function(server)
-            local lspconfig = require("lspconfig")
-            lspconfig[server].setup({
-              on_attach = on_attach,
-              capabilities = capabilities,
-            })
-          end,
-        },
-      })
-    end,
-  },
-
-  -- nvim-cmp and completion sources
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "L3MON4D3/LuaSnip",
-      "saadparwaiz1/cmp_luasnip",
-    },
-    config = function()
-      local cmp = require("cmp")
-      local luasnip = require("luasnip")
-
-      require("luasnip.loaders.from_vscode").lazy_load()
-
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump()
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-          ["<S-Tab>"] = cmp.mapping.select_prev_item(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
-          ["<C-Space>"] = cmp.mapping.complete(),
-        }),
-        sources = cmp.config.sources({
-          { name = "nvim_lsp" },
-          { name = "luasnip" },
-          { name = "buffer" },
-          { name = "path" },
-        }),
-      })
-    end,
-  },
+        end, { 'i', 's' }),
+        ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ['<C-Space>'] = cmp.mapping.complete(),
+      }),
+      sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },
+        { name = 'buffer' },
+        { name = 'path' },
+      }),
+    })
+  end,
 }
