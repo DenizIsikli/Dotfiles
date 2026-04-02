62caee0bc878b3ad16822acb651ac6b990fa795e
Deniz Isikli <s215818@dtu.dk>
2026-04-03 01:11:01 +0200

    oil hidden files removed

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,11 +1,7 @@
 return {
 	"stevearc/oil.nvim",
 	config = function()
-		require("oil").setup({
-			view_options = {
-				show_hidden = true,
-			},
-		})
+		require("oil").setup()
 		vim.keymap.set("n", "+", require("oil").open)
 	end,
 }
