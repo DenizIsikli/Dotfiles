f5bb91b3d9504fee2159aee7d1a6a5f2b0c77b01
Deniz Isikli <s215818@dtu.dk>
2026-04-15 18:32:17 +0200

    Font fallback

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,22 +2,22 @@ local wezterm = require("wezterm")
 local config = {}
 
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 wezterm.on("gui-startup", function(cmd)
-  local tab, pane, window = wezterm.mux.spawn_window(cmd or {})
-  local gui_window = window:gui_window()
+	local tab, pane, window = wezterm.mux.spawn_window(cmd or {})
+	local gui_window = window:gui_window()
 
-  local screen = wezterm.gui.screens().active
+	local screen = wezterm.gui.screens().active
 
-  local width = 180 * 8
-  local height = 50 * 18
+	local width = 180 * 8
+	local height = 50 * 18
 
-  local x = screen.x + (screen.width - width) / 2
-  local y = screen.y + (screen.height - height) / 2
+	local x = screen.x + (screen.width - width) / 2
+	local y = screen.y + (screen.height - height) / 2
 
-  gui_window:set_position(x, y)
+	gui_window:set_position(x, y)
 end)
 
 config.automatically_reload_config = true
@@ -34,27 +34,30 @@ config.initial_cols = 180
 config.initial_rows = 50
 
 config.window_padding = {
-  left = 5,
-  right = 5,
-  top = 0,
-  bottom = 0,
+	left = 5,
+	right = 5,
+	top = 0,
+	bottom = 0,
 }
 
 config.font_size = 10.5
-config.font = wezterm.font("FiraMono Nerd Font")
-config.color_scheme = 'Panda (Gogh)'
+config.font = wezterm.font_with_fallback({
+	"FiraMono Nerd Font",
+	"Symbols Nerd Font",
+	"Noto Color Emoji",
+})
+config.color_scheme = "Panda (Gogh)"
 
 config.scrollback_lines = 10000
 config.window_background_opacity = 0.8
-config.macos_window_background_blur = 10
-config.color_scheme = 'Grayscale Dark (base16)'
+config.color_scheme = "Grayscale Dark (base16)"
 
 config.default_prog = {
-  "C:\\Windows\\System32\\wsl.exe",
-  "-d",
-  "Debian",
-  "-e",
-  "tmux",
+	"C:\\Windows\\System32\\wsl.exe",
+	"-d",
+	"Debian",
+	"-e",
+	"tmux",
 }
 
 config.front_end = "WebGpu"
